98fb5e338551e3b8adbfb5c31bce82c33d2805b1
kevinthegreat1 <kaiweikevinyang2019@gmail.com>
2022-01-03 20:59:33 -0800

    Reparty

--- src/main/java/com/kevinthegreat/messagereplacer/Reparty.java
+++ src/main/java/com/kevinthegreat/messagereplacer/Reparty.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public class Reparty {
     private boolean reparty = false;
-    private final List<String> players = new ArrayList<>();
+    private int memberCount = 0;
+    private final List<String> members = new ArrayList<>();
     private String leader = "";
 
     public void startReparty() {
@@ -17,9 +18,12 @@ public class Reparty {
 
     public void onChatMessage(String message) {
         if (reparty) {
-            if (message.equals("Party Members (1)")) {
-                MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("The Party is empty."));
-                reparty = false;
+            if (message.startsWith("Party Members (")) {
+                memberCount = Integer.parseInt(message.substring(15, message.length() - 1)) - 1;
+                if (memberCount == 0) {
+                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of("The Party is empty."));
+                    reparty = false;
+                }
             } else if (message.startsWith("Party Leader")) {
                 String[] messageContents = message.split(" ");
                 MinecraftClient minecraftClient = MinecraftClient.getInstance();
@@ -32,16 +36,16 @@ public class Reparty {
                 String[] messageContents = message.split(" ");
                 for (int i = 2; i < messageContents.length - 1; i++) {
                     if (!messageContents[i].contains("[") && !messageContents[i].contains("●")) {
-                        players.add(messageContents[i]);
+                        members.add(messageContents[i]);
                         i++;
                     }
                 }
-                if (message.startsWith("Party Members:")) {
-                    String invite = "/p ";
-                    invite += String.join(" ", players);
-                    players.clear();
+                if (members.size() == memberCount) {
                     MessageReplacer.messageReplacer.queueMessage("/p disband", 200);
-                    MessageReplacer.messageReplacer.queueMessage(invite, 500);
+                    for (String player : members) {
+                        MessageReplacer.messageReplacer.queueMessage("/p invite " + player, 300);
+                    }
+                    members.clear();
                     reparty = false;
                 }
             } else if (message.startsWith("You are not currently in a party.")) {
@@ -54,13 +58,11 @@ public class Reparty {
             } else {
                 leader = messageContents[0];
             }
-        } else {
-            if (!leader.isEmpty() && message.contains(leader + " has invited you to join their party!")) {
-                assert MinecraftClient.getInstance().player != null;
-                if (!leader.equals(MinecraftClient.getInstance().player.getEntityName())) {
-                    MessageReplacer.messageReplacer.queueMessage("/p accept " + leader, 200);
-                    leader = "";
-                }
+        } else if (!leader.isEmpty() && message.contains(leader + " has invited you to join their party!")) {
+            assert MinecraftClient.getInstance().player != null;
+            if (!leader.equals(MinecraftClient.getInstance().player.getEntityName())) {
+                MessageReplacer.messageReplacer.queueMessage("/p accept " + leader, 200);
+                leader = "";
             }
         }
     }
