4a085b5b44a7a1167dfb546988ad7a8fcdbac128
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2022-06-23 22:01:04 +0800

    Experiment Solvers Chronomatron and improvements

--- src/main/java/com/kevinthegreat/skyblockmod/misc/Experiments.java
+++ src/main/java/com/kevinthegreat/skyblockmod/misc/Experiments.java
@@ -1,17 +1,15 @@
 package com.kevinthegreat.skyblockmod.misc;
 
+import com.google.common.collect.ImmutableMap;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.screen.slot.Slot;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Experiments {
     public boolean toggleChronomatron = true;
@@ -23,18 +21,35 @@ public class Experiments {
     }
 
     public enum State {
-        REMEMBER, WAIT, SHOW
+        REMEMBER, WAIT, SHOW, END
     }
 
     public Type type = Type.NONE;
     public State state = State.REMEMBER;
 
+    public final List<Item> chronomatronSlots = new ArrayList<>();
+    public int chronomatronChainLengthCount;
+    public int chronomatronCurrentSlot;
+    public int chronomatronCurrentOrdinal;
     public final Map<Integer, ItemStack> superpairsSlots = new HashMap<>();
-    public int superpairsLastClickedSlot;
+    public int superpairsPrevClickedSlot;
     public ItemStack superpairsCurrentSlot;
     public final Set<Integer> superpairsDuplicatedSlots = new HashSet<>();
     public final Map<Integer, ItemStack> ultrasequencerSlots = new HashMap<>();
-    public int ultrasequencerNextSlotIndex;
+    public int ultrasequencerNextSlot;
+
+    public static final ImmutableMap<Item, Item> terracottaToGlass = ImmutableMap.ofEntries(
+            new AbstractMap.SimpleImmutableEntry<>(Items.RED_TERRACOTTA, Items.RED_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.ORANGE_TERRACOTTA, Items.ORANGE_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.YELLOW_TERRACOTTA, Items.YELLOW_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.LIME_TERRACOTTA, Items.LIME_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.GREEN_TERRACOTTA, Items.GREEN_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.CYAN_TERRACOTTA, Items.CYAN_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.LIGHT_BLUE_TERRACOTTA, Items.LIGHT_BLUE_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.BLUE_TERRACOTTA, Items.BLUE_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.PURPLE_TERRACOTTA, Items.PURPLE_STAINED_GLASS),
+            new AbstractMap.SimpleImmutableEntry<>(Items.PINK_TERRACOTTA, Items.PINK_STAINED_GLASS)
+    );
 
     public Experiments() {
         ScreenEvents.AFTER_INIT.register((minecraftClient, screen, scaledWidth, scaledHeight) -> {
@@ -72,73 +87,124 @@ public class Experiments {
 
     public void tick() {
         if (type != Type.NONE) {
-            Screen currentScreen = MinecraftClient.getInstance().currentScreen;
-            if (currentScreen instanceof GenericContainerScreen genericContainerScreen) {
-                String title = genericContainerScreen.getTitle().getString();
-                switch (type) {
-                    case CHRONOMATRON -> {
-                        if (toggleChronomatron && title.startsWith("Chronomatron (")) {
-                            chronomatron(genericContainerScreen);
-                        } else {
-                            reset();
-                        }
-                    }
-                    case SUPERPAIRS -> {
-                        if (toggleSuperpairs && title.startsWith("Superpairs (")) {
-                            superpairs(genericContainerScreen);
-                        } else {
-                            reset();
-                        }
-                    }
-                    case ULTRASEQUENCER -> {
-                        if (toggleUltrasequencer && title.startsWith("Ultrasequencer (")) {
-                            ultrasequencer(genericContainerScreen);
-                        } else {
-                            reset();
-                        }
+            switch (type) {
+                case CHRONOMATRON -> {
+                    if (toggleChronomatron && MinecraftClient.getInstance().currentScreen instanceof GenericContainerScreen genericContainerScreen && genericContainerScreen.getTitle().getString().startsWith("Chronomatron (")) {
+                        chronomatron(genericContainerScreen);
+                    } else {
+                        resetChronomatron();
                     }
                 }
-            } else {
-                reset();
+                case SUPERPAIRS -> {
+                    if (toggleSuperpairs && MinecraftClient.getInstance().currentScreen instanceof GenericContainerScreen genericContainerScreen && genericContainerScreen.getTitle().getString().startsWith("Superpairs (")) {
+                        superpairs(genericContainerScreen);
+                    } else {
+                        resetSuperpairs();
+                    }
+                }
+                case ULTRASEQUENCER -> {
+                    if (toggleUltrasequencer && MinecraftClient.getInstance().currentScreen instanceof GenericContainerScreen genericContainerScreen && genericContainerScreen.getTitle().getString().startsWith("Ultrasequencer (")) {
+                        ultrasequencer(genericContainerScreen);
+                    } else {
+                        resetUltrasequencer();
+                    }
+                }
             }
         }
+    }
+
+    private void resetChronomatron() {
+        chronomatronSlots.clear();
+        chronomatronChainLengthCount = 0;
+        chronomatronCurrentSlot = 0;
+        chronomatronCurrentOrdinal = 0;
+        reset();
+    }
+
+    private void resetSuperpairs() {
+        superpairsSlots.clear();
+        reset();
+    }
+
+    private void resetUltrasequencer() {
+        ultrasequencerSlots.clear();
+        reset();
     }
 
     private void reset() {
         type = Type.NONE;
         state = State.REMEMBER;
-        superpairsSlots.clear();
-        ultrasequencerSlots.clear();
     }
 
     private void chronomatron(GenericContainerScreen screen) {
+        switch (state) {
+            case REMEMBER -> {
+                Inventory inventory = screen.getScreenHandler().getInventory();
+                if (chronomatronCurrentSlot == 0) {
+                    for (int index = 10; index < 43; index++) {
+                        if (inventory.getStack(index).hasEnchantments()) {
+                            if (chronomatronSlots.size() <= chronomatronChainLengthCount) {
+                                chronomatronSlots.add(terracottaToGlass.get(inventory.getStack(index).getItem()));
+                                state = State.WAIT;
+                            } else {
+                                chronomatronChainLengthCount++;
+                            }
+                            chronomatronCurrentSlot = index;
+                            return;
+                        }
+                    }
+                } else if (!inventory.getStack(chronomatronCurrentSlot).hasEnchantments()) {
+                    chronomatronCurrentSlot = 0;
+                }
+            }
+            case WAIT -> {
+                if (screen.getScreenHandler().getInventory().getStack(49).getName().getString().startsWith("Timer: ")) {
+                    state = State.SHOW;
+                }
+            }
+            case END -> {
+                String name = screen.getScreenHandler().getInventory().getStack(49).getName().getString();
+                if (!name.startsWith("Timer: ")) {
+                    if (name.equals("Remember the pattern!")) {
+                        chronomatronChainLengthCount = 0;
+                        chronomatronCurrentOrdinal = 0;
+                        state = State.REMEMBER;
+                    } else {
+                        resetChronomatron();
+                    }
+                }
+            }
+        }
     }
 
     private void superpairs(GenericContainerScreen screen) {
-        if (state == State.SHOW && superpairsSlots.get(superpairsLastClickedSlot) == null) {
-            ItemStack itemStack = screen.getScreenHandler().getInventory().getStack(superpairsLastClickedSlot);
-            if (!(itemStack.isOf(Items.CYAN_STAINED_GLASS) || itemStack.isOf(Items.BLACK_STAINED_GLASS_PANE) || itemStack.isOf(Items.AIR))) {
-                superpairsSlots.entrySet().stream().filter((entry -> ItemStack.areEqual(entry.getValue(), itemStack))).findAny().ifPresent(entry -> superpairsDuplicatedSlots.add(entry.getKey()));
-                superpairsSlots.put(superpairsLastClickedSlot, itemStack);
-                superpairsCurrentSlot = itemStack;
+        if (state == State.SHOW) {
+            if (screen.getScreenHandler().getInventory().getStack(4).isOf(Items.CAULDRON)) {
+                resetSuperpairs();
+            } else if (superpairsSlots.get(superpairsPrevClickedSlot) == null) {
+                ItemStack itemStack = screen.getScreenHandler().getInventory().getStack(superpairsPrevClickedSlot);
+                if (!(itemStack.isOf(Items.CYAN_STAINED_GLASS) || itemStack.isOf(Items.BLACK_STAINED_GLASS_PANE) || itemStack.isOf(Items.AIR))) {
+                    superpairsSlots.entrySet().stream().filter((entry -> ItemStack.areEqual(entry.getValue(), itemStack))).findAny().ifPresent(entry -> superpairsDuplicatedSlots.add(entry.getKey()));
+                    superpairsSlots.put(superpairsPrevClickedSlot, itemStack);
+                    superpairsCurrentSlot = itemStack;
+                }
             }
-        }
-        if (screen.getScreenHandler().getInventory().getStack(4).isOf(Items.CAULDRON)) {
-            reset();
         }
     }
 
     private void ultrasequencer(GenericContainerScreen screen) {
         switch (state) {
             case REMEMBER -> {
-                if (screen.getScreenHandler().getInventory().getStack(49).getName().getString().equals("Remember the pattern!")) {
-                    for (Slot slot : screen.getScreenHandler().slots) {
-                        String name = slot.getStack().getName().getString();
+                Inventory inventory = screen.getScreenHandler().getInventory();
+                if (inventory.getStack(49).getName().getString().equals("Remember the pattern!")) {
+                    for (int index = 9; index < 45; index++) {
+                        ItemStack itemStack = inventory.getStack(index);
+                        String name = itemStack.getName().getString();
                         if (name.matches("\\d+")) {
                             if (name.equals("1")) {
-                                ultrasequencerNextSlotIndex = slot.getIndex();
+                                ultrasequencerNextSlot = index;
                             }
-                            ultrasequencerSlots.put(slot.getIndex(), slot.getStack());
+                            ultrasequencerSlots.put(index, itemStack);
                         }
                     }
                     state = State.WAIT;
@@ -149,10 +215,15 @@ public class Experiments {
                     state = State.SHOW;
                 }
             }
-            case SHOW -> {
-                if (!screen.getScreenHandler().getInventory().getStack(49).getName().getString().startsWith("Timer: ")) {
-                    ultrasequencerSlots.clear();
-                    state = State.REMEMBER;
+            case END -> {
+                String name = screen.getScreenHandler().getInventory().getStack(49).getName().getString();
+                if (!name.startsWith("Timer: ")) {
+                    if (name.equals("Remember the pattern!")) {
+                        ultrasequencerSlots.clear();
+                        state = State.REMEMBER;
+                    } else {
+                        resetUltrasequencer();
+                    }
                 }
             }
         }
--- src/main/java/com/kevinthegreat/skyblockmod/mixins/HandledScreenMixin.java
+++ src/main/java/com/kevinthegreat/skyblockmod/mixins/HandledScreenMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
@@ -15,33 +16,43 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Map;
+import java.util.Optional;
+
 @Mixin(HandledScreen.class)
 public class HandledScreenMixin {
+    final Experiments experiments = SkyblockMod.skyblockMod.experiments;
 
     @Inject(method = "drawSlot", at = @At(value = "TAIL"))
     private void fillSlot(MatrixStack matrices, Slot slot, CallbackInfo ci) {
-        Experiments experiments = SkyblockMod.skyblockMod.experiments;
-        if ((experiments.toggleChronomatron || experiments.toggleSuperpairs || experiments.toggleUltrasequencer) && experiments.type != Experiments.Type.NONE && experiments.state == Experiments.State.SHOW && slot.inventory instanceof SimpleInventory) {
+        if (experiments.type != Experiments.Type.NONE && experiments.state == Experiments.State.SHOW && slot.inventory instanceof SimpleInventory) {
             switch (experiments.type) {
+                case CHRONOMATRON -> {
+                    Item item = experiments.chronomatronSlots.get(experiments.chronomatronCurrentOrdinal);
+                    if (slot.getStack().isOf(item) || Experiments.terracottaToGlass.get(slot.getStack().getItem()) == item) {
+                        matrices.push();
+                        matrices.translate(0, 0, 300);
+                        DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, -1073676544);
+                        matrices.pop();
+                    }
+                }
                 case SUPERPAIRS -> {
-                    if (experiments.toggleSuperpairs) {
-                        ItemStack itemStack = experiments.superpairsSlots.get(slot.getIndex());
-                        if (itemStack != null && !ItemStack.areEqual(itemStack, slot.getStack())) {
-                            matrices.push();
-                            matrices.translate(0, 0, 300);
-                            if (ItemStack.areEqual(experiments.superpairsCurrentSlot, itemStack) && slot.getStack().getName().getString().equals("Click a second button!")) {
-                                DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, -1073676544);
-                            } else if (experiments.superpairsDuplicatedSlots.contains(slot.getIndex())) {
-                                DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, -1056964864);
-                            } else {
-                                DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, 1090453504);
-                            }
-                            matrices.pop();
+                    ItemStack itemStack = experiments.superpairsSlots.get(slot.getIndex());
+                    if (itemStack != null && !ItemStack.areEqual(itemStack, slot.getStack())) {
+                        matrices.push();
+                        matrices.translate(0, 0, 300);
+                        if (ItemStack.areEqual(experiments.superpairsCurrentSlot, itemStack) && slot.getStack().getName().getString().equals("Click a second button!")) {
+                            DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, -1073676544);
+                        } else if (experiments.superpairsDuplicatedSlots.contains(slot.getIndex())) {
+                            DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, -1056964864);
+                        } else {
+                            DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, 1090453504);
                         }
+                        matrices.pop();
                     }
                 }
                 case ULTRASEQUENCER -> {
-                    if (experiments.toggleUltrasequencer && slot.getIndex() == experiments.ultrasequencerNextSlotIndex) {
+                    if (slot.getIndex() == experiments.ultrasequencerNextSlot) {
                         matrices.push();
                         matrices.translate(0, 0, 300);
                         DrawableHelper.fill(matrices, slot.x, slot.y, slot.x + 16, slot.y + 16, -1073676544);
@@ -54,27 +65,22 @@ public class HandledScreenMixin {
 
     @Redirect(method = "drawSlot", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/slot/Slot;getStack()Lnet/minecraft/item/ItemStack;", ordinal = 0))
     private ItemStack getStack(Slot slot) {
-        Experiments experiments = SkyblockMod.skyblockMod.experiments;
-        if ((experiments.toggleChronomatron || experiments.toggleSuperpairs || experiments.toggleUltrasequencer) && experiments.type != Experiments.Type.NONE && experiments.state == Experiments.State.SHOW && slot.inventory instanceof SimpleInventory) {
+        if (experiments.type != Experiments.Type.NONE && experiments.state == Experiments.State.SHOW && slot.inventory instanceof SimpleInventory) {
             switch (experiments.type) {
                 case SUPERPAIRS -> {
-                    if (experiments.toggleSuperpairs) {
-                        ItemStack itemStack = experiments.superpairsSlots.get(slot.getIndex());
-                        return itemStack == null ? slot.getStack() : itemStack;
-                    }
+                    ItemStack itemStack = experiments.superpairsSlots.get(slot.getIndex());
+                    return itemStack == null ? slot.getStack() : itemStack;
                 }
                 case ULTRASEQUENCER -> {
-                    if (experiments.toggleUltrasequencer) {
-                        ItemStack itemStack = experiments.ultrasequencerSlots.get(slot.getIndex());
-                        return itemStack == null ? slot.getStack() : itemStack;
-                    }
+                    ItemStack itemStack = experiments.ultrasequencerSlots.get(slot.getIndex());
+                    return itemStack == null ? slot.getStack() : itemStack;
                 }
             }
         }
         return slot.getStack();
     }
 
-    @Redirect(method = "drawMouseoverTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/slot/Slot;getStack()Lnet/minecraft/item/ItemStack;", ordinal = 0))
+    @Redirect(method = "drawMouseoverTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/slot/Slot;getStack()Lnet/minecraft/item/ItemStack;"))
     private ItemStack getStackTooltip(Slot slot) {
         return getStack(slot);
     }
@@ -82,19 +88,29 @@ public class HandledScreenMixin {
     @Inject(method = "onMouseClick(Lnet/minecraft/screen/slot/Slot;IILnet/minecraft/screen/slot/SlotActionType;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;clickSlot(IIILnet/minecraft/screen/slot/SlotActionType;Lnet/minecraft/entity/player/PlayerEntity;)V"))
     private void onSlotClick(Slot slot, int slotId, int button, SlotActionType actionType, CallbackInfo ci) {
         if (slot != null) {
-            Experiments experiments = SkyblockMod.skyblockMod.experiments;
-            if ((experiments.toggleChronomatron || experiments.toggleSuperpairs || experiments.toggleUltrasequencer) && experiments.type != Experiments.Type.NONE && experiments.state == Experiments.State.SHOW && slot.inventory instanceof SimpleInventory) {
+            if (experiments.type != Experiments.Type.NONE && experiments.state == Experiments.State.SHOW && slot.inventory instanceof SimpleInventory) {
                 switch (experiments.type) {
-                    case SUPERPAIRS -> {
-                        if (experiments.toggleSuperpairs) {
-                            experiments.superpairsLastClickedSlot = slot.getIndex();
-                            experiments.superpairsCurrentSlot = ItemStack.EMPTY;
+                    case CHRONOMATRON -> {
+                        Item item = experiments.chronomatronSlots.get(experiments.chronomatronCurrentOrdinal);
+                        if (slot.getStack().isOf(item) || Experiments.terracottaToGlass.get(slot.getStack().getItem()) == item) {
+                            if (++experiments.chronomatronCurrentOrdinal >= experiments.chronomatronSlots.size()) {
+                                experiments.state = Experiments.State.END;
+                            }
                         }
                     }
+                    case SUPERPAIRS -> {
+                        experiments.superpairsPrevClickedSlot = slot.getIndex();
+                        experiments.superpairsCurrentSlot = ItemStack.EMPTY;
+                    }
                     case ULTRASEQUENCER -> {
-                        if (experiments.toggleUltrasequencer && slot.getIndex() == experiments.ultrasequencerNextSlotIndex) {
-                            int count = experiments.ultrasequencerSlots.get(experiments.ultrasequencerNextSlotIndex).getCount() + 1;
-                            experiments.ultrasequencerSlots.entrySet().stream().filter(entry -> entry.getValue().getCount() == count).findAny().ifPresent(entry -> experiments.ultrasequencerNextSlotIndex = entry.getKey());
+                        if (slot.getIndex() == experiments.ultrasequencerNextSlot) {
+                            int count = experiments.ultrasequencerSlots.get(experiments.ultrasequencerNextSlot).getCount() + 1;
+                            Optional<Map.Entry<Integer, ItemStack>> ultrasequencerNextSlotEntry = experiments.ultrasequencerSlots.entrySet().stream().filter(entry -> entry.getValue().getCount() == count).findAny();
+                            if (ultrasequencerNextSlotEntry.isPresent()) {
+                                experiments.ultrasequencerNextSlot = ultrasequencerNextSlotEntry.get().getKey();
+                            } else {
+                                experiments.state = Experiments.State.END;
+                            }
                         }
                     }
                 }
