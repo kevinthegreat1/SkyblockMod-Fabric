31e0f1cde83273fe3a40d32be8edc05bed3d3cea
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-03-26 02:00:39 -0400

    Added guiding dust detection

--- src/main/java/com/kevinthegreat/skyblockmod/misc/MythologicalRitual.java
+++ src/main/java/com/kevinthegreat/skyblockmod/misc/MythologicalRitual.java
@@ -7,40 +7,68 @@ import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.DyeColor;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.*;
 
 public class MythologicalRitual {
     private static final int PARTICLE_TIMEOUT = 10;
-    private final Map<BlockPos, IntIntMutablePair> particlesMap = new HashMap<>();
+    private static final int GUIDING_DUST_SIZE = 20;
+    private final Map<BlockPos, IntIntMutablePair> potentialGriffinBurrows = new HashMap<>();
     private final Set<BlockPos> griffinBurrows = new HashSet<>();
+    private final Map<BlockPos, GuidingDust> potentialGuidingDusts = new HashMap<>();
+    private final Map<BlockPos, Pair<Vec3d, Vec3d>> guidingDusts = new HashMap<>();
 
     public void onParticle(ParticleS2CPacket packet) {
-        if (SkyblockMod.skyblockMod.options.mythologicalRitual.getValue() && ParticleTypes.CRIT.equals(packet.getParameters().getType()) || ParticleTypes.ENCHANT.equals(packet.getParameters().getType())) {
+        if (SkyblockMod.skyblockMod.options.mythologicalRitual.getValue()) {
             BlockPos pos = BlockPos.ofFloored(packet.getX(), packet.getY(), packet.getZ());
-            IntIntMutablePair particlesAtPos = particlesMap.computeIfAbsent(pos, pos1 -> IntIntMutablePair.of(0, 0));
-            if (ParticleTypes.CRIT.equals(packet.getParameters().getType())) {
-                particlesAtPos.left(PARTICLE_TIMEOUT);
-            } else if (ParticleTypes.ENCHANT.equals(packet.getParameters().getType())) {
-                particlesAtPos.right(PARTICLE_TIMEOUT);
-            }
-            if (particlesAtPos.leftInt() > 0 && particlesAtPos.rightInt() > 0) {
-                griffinBurrows.add(pos);
+            if (ParticleTypes.CRIT.equals(packet.getParameters().getType()) || ParticleTypes.ENCHANT.equals(packet.getParameters().getType())) {
+                IntIntMutablePair particlesAtPos = potentialGriffinBurrows.computeIfAbsent(pos, pos1 -> IntIntMutablePair.of(0, 0));
+                if (ParticleTypes.CRIT.equals(packet.getParameters().getType())) {
+                    particlesAtPos.left(PARTICLE_TIMEOUT);
+                } else if (ParticleTypes.ENCHANT.equals(packet.getParameters().getType())) {
+                    particlesAtPos.right(PARTICLE_TIMEOUT);
+                }
+                if (particlesAtPos.leftInt() > 0 && particlesAtPos.rightInt() > 0) {
+                    griffinBurrows.add(pos);
+                }
+            } else if (ParticleTypes.DUST.equals(packet.getParameters().getType())) {
+                SkyblockMod.LOGGER.info("Received dust at: " + pos + " with offset: " + packet.getOffsetX() + ", " + packet.getOffsetY() + ", " + packet.getOffsetZ()); //FIXME remove
+                GuidingDust guidingDust = potentialGuidingDusts.computeIfAbsent(pos, pos1 -> new GuidingDust());
+                guidingDust.addDust(packet.getX(), packet.getY(), packet.getZ());
+                guidingDust.resetTimeout();
+                if (guidingDust.hasEnoughDust()) {
+                    guidingDust.calculateRenderingPlane();
+                    SkyblockMod.LOGGER.info("Adding confirmed guiding dusts at: " + pos + " with rendering plane pos 1: " + guidingDust.getRenderingPlane().getLeft() + " and pos 2: " + guidingDust.getRenderingPlane().getRight()); //FIXME remove
+                    guidingDusts.put(pos, guidingDust.getRenderingPlane());
+                }
             }
         }
     }
 
     public void tick() {
         if (SkyblockMod.skyblockMod.options.mythologicalRitual.getValue()) {
-            for (Iterator<Map.Entry<BlockPos, IntIntMutablePair>> posParticleIt = particlesMap.entrySet().iterator(); posParticleIt.hasNext(); ) {
-                BlockPos pos = posParticleIt.next().getKey();
-                IntIntMutablePair particlesAtPos = particlesMap.get(pos);
+            for (Iterator<Map.Entry<BlockPos, IntIntMutablePair>> potentialGriffinBurrowIt = potentialGriffinBurrows.entrySet().iterator(); potentialGriffinBurrowIt.hasNext(); ) {
+                Map.Entry<BlockPos, IntIntMutablePair> potentialGriffinBurrow = potentialGriffinBurrowIt.next();
+                BlockPos pos = potentialGriffinBurrow.getKey();
+                IntIntMutablePair particlesAtPos = potentialGriffinBurrow.getValue();
                 particlesAtPos.left(Math.max(0, particlesAtPos.leftInt() - 1));
                 particlesAtPos.right(Math.max(0, particlesAtPos.rightInt() - 1));
                 if (particlesAtPos.leftInt() + particlesAtPos.rightInt() <= 0) {
                     griffinBurrows.remove(pos);
-                    posParticleIt.remove();
+                    potentialGriffinBurrowIt.remove();
+                }
+            }
+            for (Iterator<Map.Entry<BlockPos, GuidingDust>> potentialGuidingDustIt = potentialGuidingDusts.entrySet().iterator(); potentialGuidingDustIt.hasNext(); ) {
+                Map.Entry<BlockPos, GuidingDust> potentialGuidingDust = potentialGuidingDustIt.next();
+                BlockPos pos = potentialGuidingDust.getKey();
+                GuidingDust guidingDust = potentialGuidingDust.getValue();
+                guidingDust.tick();
+                if (guidingDust.isExpired()) {
+                    guidingDusts.remove(pos);
+                    potentialGuidingDustIt.remove();
                 }
             }
         }
@@ -51,6 +79,70 @@ public class MythologicalRitual {
             for (BlockPos griffinBorrow : griffinBurrows) {
                 RenderHelper.renderFilledThroughWallsWithBeaconBeam(context, griffinBorrow, DyeColor.GREEN.getColorComponents(), 0.5F);
             }
+            for (Map.Entry<BlockPos, Pair<Vec3d, Vec3d>> guidingDust : guidingDusts.entrySet()) {
+                RenderHelper.renderVerticalPlane(context, guidingDust.getKey(), guidingDust.getValue().getLeft(), guidingDust.getValue().getRight(), DyeColor.GREEN.getColorComponents(), 0.5F);
+            }
+        }
+    }
+
+    private static class GuidingDust {
+        private final Set<Vec3d> dusts = new HashSet<>();
+        private Vec3d renderPlanePos1;
+        private Vec3d renderPlanePos2;
+        private int timeout = PARTICLE_TIMEOUT;
+
+        public void addDust(double x, double y, double z) {
+            dusts.add(new Vec3d(x, y, z));
+        }
+
+        public boolean hasEnoughDust() {
+            return dusts.size() >= GUIDING_DUST_SIZE;
+        }
+
+        public void tick() {
+            timeout = Math.max(0, timeout - 1);
+        }
+
+        public void resetTimeout() {
+            timeout = PARTICLE_TIMEOUT;
+        }
+
+        public boolean isExpired() {
+            return timeout <= 0;
+        }
+
+        public Pair<Vec3d, Vec3d> getRenderingPlane() {
+            if (renderPlanePos1 == null || renderPlanePos2 == null) {
+                calculateRenderingPlane();
+            }
+            return new Pair<>(renderPlanePos1, renderPlanePos2);
+        }
+
+        public void calculateRenderingPlane() {
+            Vec3d bestFitVerticalPlane = calculateBestFitVerticalPlane();
+            renderPlanePos1 = bestFitVerticalPlane.multiply(-1); //FIXME use large number (256?) for multiply
+            renderPlanePos2 = bestFitVerticalPlane.multiply(1);
+        }
+
+        private Vec3d calculateBestFitVerticalPlane() {
+            int points = 0;
+            double meanX = 0;
+            double meanZ = 0;
+            double meanXSquared = 0;
+            double meanXZ = 0;
+            for (Vec3d dust : dusts) {
+                points++;
+                meanX += dust.x;
+                meanZ += dust.z;
+                meanXSquared += dust.x * dust.x;
+                meanXZ += dust.x * dust.z;
+            }
+            meanX /= points;
+            meanZ /= points;
+            meanXSquared /= points;
+            meanXZ /= points;
+            double slope = (meanXZ - meanX * meanZ) / (meanXSquared - meanX * meanX);
+            return new Vec3d(1, 0, slope).normalize();
         }
     }
 }
--- src/main/java/com/kevinthegreat/skyblockmod/util/RenderHelper.java
+++ src/main/java/com/kevinthegreat/skyblockmod/util/RenderHelper.java
@@ -1,11 +1,15 @@
 package com.kevinthegreat.skyblockmod.util;
 
+import com.kevinthegreat.skyblockmod.SkyblockMod;
 import com.kevinthegreat.skyblockmod.mixins.accessors.BeaconBlockEntityRendererInvoker;
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.x150.renderer.render.Renderer3d;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import org.joml.Matrix4f;
 
 import java.awt.*;
 
@@ -26,5 +30,32 @@ public class RenderHelper {
         context.matrixStack().translate(pos.getX() - context.camera().getPos().x, pos.getY() - context.camera().getPos().y, pos.getZ() - context.camera().getPos().z);
         BeaconBlockEntityRendererInvoker.renderBeam(context.matrixStack(), context.consumers(), context.tickDelta(), context.world().getTime(), 0, BeaconBlockEntityRenderer.MAX_BEAM_HEIGHT, colorComponents);
         context.matrixStack().pop();
+    }
+
+    public static void renderVerticalPlane(WorldRenderContext context, BlockPos pos, Vec3d pos1, Vec3d pos2, float[] colorComponents, float alpha) { //FIXME why not work?
+        SkyblockMod.LOGGER.info("Rendering vertical plane at pos: " + pos + " with pos1: " + pos1 + " and pos2: " + pos2); //FIXME remove
+        RenderSystem.disableDepthTest();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        context.matrixStack().push();
+        context.matrixStack().translate(pos.getX() - context.camera().getPos().x, pos.getY() - context.camera().getPos().y, pos.getZ() - context.camera().getPos().z);
+        BufferBuilder buffer = Tessellator.getInstance().getBuffer();
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+
+        Matrix4f matrix = context.matrixStack().peek().getPositionMatrix();
+        float x1 = (float) pos1.x;
+        float z1 = (float) pos1.z;
+        float x2 = (float) pos2.x;
+        float z2 = (float) pos2.z;
+        float red = colorComponents[0];
+        float green = colorComponents[1];
+        float blue = colorComponents[2];
+        buffer.vertex(matrix, x1, -1, z1).color(red, green, blue, alpha).next(); //FIXME use large number (256?) for y
+        buffer.vertex(matrix, x1, 1, z1).color(red, green, blue, alpha).next();
+        buffer.vertex(matrix, x2, 1, z2).color(red, green, blue, alpha).next();
+        buffer.vertex(matrix, x2, -1, z2).color(red, green, blue, alpha).next();
+
+        BufferRenderer.drawWithGlobalProgram(buffer.end());
+        context.matrixStack().pop();
+        RenderSystem.enableDepthTest();
     }
 }
